9fceaf28320ce38a93b166cc10de3ae0a1c6b6e8
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-11-08 18:40:19 -0300

    Inicia projeto e cria connection com DB

---
+++ src/models/connection.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const MONGO_DB_URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/Cookmaster`;
+const DB_NAME = 'Cookmaster';
+
+let db = null;
+
+const connection = () => (db
+    ? Promise.resolve(db)
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+      .then((conn) => {
+        db = conn.db(DB_NAME);
+        return db;
+      }));
+
+module.exports = connection;
